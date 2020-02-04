49e4b133ec825a8ac83383fb2beecc7742d3699d
Золотухин Сергей Иванович <zolotukhinsi@mosreg.ru>
2020-02-04 18:10:37 +0300

    Cosmetics.

--- api/sections/models.py
+++ api/sections/models.py
@@ -4,12 +4,21 @@ from libs.models import BaseModel
 class Section(BaseModel):
     """
     Section item model.
-        id - идентификатор
         parent_id - Ссылка на родителя
-        title - заголовок раздела
         enabled - флаг "раздел доступен на сайте"
+        order_key - ключ сортировки
+
+        title - заголовок раздела
+        description - описание
     """
 
     class Meta:
-        fields = ('parent_id', 'enabled', 'order_key')
-        translations = ('title', 'description')
+        fields = (
+            'parent_id',
+            'enabled',
+            'order_key'
+        )
+        translations = (
+            'title',
+            'description'
+        )
